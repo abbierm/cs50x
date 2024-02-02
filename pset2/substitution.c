f523994a056630578185d8d1b18af2a4ea7611c0
abbierm <106941149+abbierm@users.noreply.github.com>
2024-02-01 19:25:18 -0600

    Update substitution.c
    Redo

--- pset2/substitution.c
+++ pset2/substitution.c
@@ -1,90 +1,121 @@
 #include <cs50.h>
-#include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct KeyMap {
-	char og[52];
-	char ng[52];
+struct keyMap
+{
+    char c[52];
+    char s[52];
 };
 
-int check_key(string key);
-struct KeyMap make_keymap(string user_key);
-void sub(struct KeyMap map, string plaintext);
+bool check_argument_count(int args);
+bool check_key_length(string key);
+bool check_key(string key);
+struct keyMap make_map(string key);
+void cipher_text(struct keyMap keymap, string text);
 
-int main(int argc, string argv[]){
+int main(int argc, string argv[])
+{
+    if (!check_argument_count(argc))
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
 
-	// Check number of cmd arguments
-	if (argc != 2){
-		printf("Invalid number of arguments\n");
-		return 1;
-	}
+    if (!check_key(argv[1]) || !check_key_length(argv[1]))
+    {
+        printf("Key must contain all 26 characters\n");
+        return 1;
+    }
 
-	// check key has all letters and no extras/repeats
-	int key_flag = check_key(argv[1]);
-	if (key_flag != 0){
-		printf("Invalid key\n");
-		return 1;
-	}
-	struct KeyMap usermap = make_keymap(argv[1]);
-	string text = get_string("Plain text:  \n");
-	printf("ciphertext: ");
+    string text = get_string("Text: ");
 
-	sub(usermap, text);
-	printf("\n");
-	return 0;
+    string k = argv[1];
+    struct keyMap map = make_map(k);
+    printf("ciphertext: ");
+    cipher_text(map, text);
 
+    return 0;
 }
 
-int check_key(string key){
-
-	int x = strlen(key);
-	if (x != 26){
-		return 1;
-	}
-	for (int i = 0; i < 26; i++){
-		if (isalpha(key[i]) == 0){
-			return 1;
-		}
-		char c = tolower(key[i]);
-		for (int j = i + 1; j < 26; j++){
-			char compare = tolower(key[j]);
-			if (compare == c)
-			{
-				return 1;
-			}
-		}
-	}
-
-	return 0;
+bool check_argument_count(int args)
+{
+    if (args != 2)
+    {
+        return false;
+    }
+    return true;
 }
 
-struct KeyMap make_keymap(string user_key){
-	struct KeyMap user_map;
-	for (int i = 0; i < 26; i++){
-		user_map.og[i] = i + 65;
-		user_map.og[i + 26] = i + 97;
-		user_map.ng[i] = toupper(user_key[i]);
-		user_map.ng[i + 26] = tolower(user_key[i]);
-	}
-	return user_map;
+bool check_key_length(string key)
+{
+    int n = strlen(key);
+    if (n == 26)
+    {
+        return true;
+    }
+    return false;
 }
 
-void sub(struct KeyMap map, string plaintext){
-	for (int i = 0, n = strlen(plaintext); i < n; i++){
-		char c = plaintext[i];
-		if (c < 60 || c > 122 || (c > 90 && c < 97)){
-			printf("%c", c);
-		}
-		else {
-			for (int j = 0; j < 52; j++){
-				if (c == map.og[j]){
-					printf("%c", map.ng[j]);
-				}
-			}
-		}
-	}
+bool check_key(string key)
+{
+    for (int i = 0, n = strlen(key); i < n; i++)
+    {
+        char c = key[i];
+        if (isalpha(c) == 0)
+        {
+            return false;
+        }
+        for (int j = i + 1; j <= n; j++)
+        {
+            char comp = key[j];
+            if (c == comp)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
+struct keyMap make_map(string key)
+{
+    struct keyMap new_map;
+    for (int i = 0; i < 26; i++)
+    {
+        new_map.c[i] = i + 65;
+        new_map.c[i + 26] = 97 + i;
+        new_map.s[i] = toupper(key[i]);
+        new_map.s[i + 26] = tolower(key[i]);
+    }
+
+    return new_map;
+}
+
+void cipher_text(struct keyMap keymap, string text)
+{
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        char c = text[i];
+        if (c >= 65 && c <= 90 | c >= 97 && c <= 122)
+        {
+            for (int j = 0; j <= 52; j++)
+            {
+                if (c == keymap.c[j])
+                {
+                    printf("%c", keymap.s[j]);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            printf("%c", c);
+        }
+    }
+    printf("\n");
+    return;
 }
 
