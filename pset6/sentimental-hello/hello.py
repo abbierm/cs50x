1729457abddc898c279e094346ec5bce10724d97
Abbie Miller <106941149+abbierm@users.noreply.github.com>
2024-03-16 16:40:07 -0500

    Update hello.py

--- pset6/sentimental-hello/hello.py
+++ pset6/sentimental-hello/hello.py
@@ -1,9 +1,12 @@
-"""
-hello.py is a simple python program that asks user for their name and responds by saying hello.
-"""
+def get_name() -> str:
+    name = input('What is your name? ')
+    return name
 
-# CS50 function that gets user input in the form of a string
-from cs50 import get_string
 
-name = get_string("What is your name? ")
-print(f"hello, {name}")
+def print_response(name: str) -> None:
+    print(f"hello, {name}")
+
+
+if __name__ == "__main__":
+    name = get_name()
+    print_response(name)
