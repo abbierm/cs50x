064c3a5eb84ccf84708c65e9e1c9fb1816d29e0c
Abbie Miller <106941149+abbierm@users.noreply.github.com>
2024-03-16 16:41:37 -0500

    Update credit.c

--- pset1/credit.c
+++ pset1/credit.c
@@ -1,128 +1,122 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+
+bool check_sum(long n);
+int get_length(long n);
+bool check_amex(int length, long cc);
+bool check_visa(int length, long cc);
+bool check_mastercard(int length, long cc);
 
 int main(void)
 {
-    // Flag that when = 0, keeps the program running
-    int flag = 0;
-
-    // Take users credit card number
-    long USERNUM;
-    USERNUM = get_long("Input your credit card number:  \n");
-
-    int count = 0; // Counter I use to determin the numbers in the CC
-
-    // While statement that if = 1, turns loop off and prints INVALID/ VISA/ MASTERCARD/ or AMEX
-    while (flag == 0)
+    long cc_number = get_long("Number: ");
+    if (!check_sum(cc_number))
     {
-        // Make sure it is 15, 13, or 16 digits long
-        long counter = USERNUM;
-        do
-        {
-            counter /= 10;
-            ++count;
-        }
-        while (counter != 0);
-
-        if (count == 15 || count == 13 || count == 16)
-        {
-            // check sum
-            long luhns_algo = USERNUM;
-            int even_sum = 0;
-            int odd_sum = 0;
-
-            while (luhns_algo > 0)
-            {
-                // Odd Digits
-                int sum1 = 0;
-                sum1 = luhns_algo % 10;
-                luhns_algo /= 10;
-                odd_sum += sum1;
-
-                // Even Digits
-                int multiply_two = 0;
-                multiply_two = luhns_algo % 10;
-                multiply_two *= 2;
-
-                // Adds the digits in multiple of two together
-                int add_digit = 0;
-                while (multiply_two > 0)
-                {
-                    add_digit += multiply_two % 10;
-                    multiply_two /= 10;
-                }
-                even_sum += add_digit;
-                luhns_algo /= 10;
-
-            }
-
-            // Adds even + odd together
-            int sum_total = 0;
-            sum_total = even_sum + odd_sum;
-
-            // Checksum
-            if (sum_total % 10 == 0)
-            {
-                long cc_type_number = USERNUM;
-                int first_digits; // variable to store beginning cc digits
-
-                // AMEX 15 DIGITS
-                if (count == 15)
-                {
-                    first_digits = cc_type_number / 10000000000000;
-                    if (first_digits == 37 | first_digits == 34)
-                    {
-                        printf("AMEX\n");
-                        flag = 1;
-                    }
-                }
-
-                else if (count == 13)
-                {
-                    first_digits = cc_type_number / 1000000000000;
-                    if (first_digits == 4)
-                    {
-                        printf("VISA\n");
-                        flag = 1;
-                    }
-                    else
-                    {
-                        printf("INVALID\n");
-                        flag = 1;
-                    }
-                }
-                else
-                {
-                    first_digits = cc_type_number / 100000000000000;
-                    if (first_digits >= 51 && first_digits <= 55)
-                    {
-                        printf("MASTERCARD\n");
-                        flag = 1;
-                    }
-                    else if (first_digits >= 40 && first_digits <= 49)
-                    {
-                        printf("VISA\n");
-                        flag = 1;
-                    }
-                    else
-                    {
-                        printf("INVALID\n");
-                        flag = 1;
-                    }
-                }
-
-            }
-            else
-            {
-                printf("INVALID\n");
-                flag = 1;
-            }
-        }
-        else
-        {
-            flag = 1;
-            printf("INVALID\n");
-        }
+        printf("INVALID\n");
+        return 0;
     }
+    int length = get_length(cc_number);
+    if (check_amex(length, cc_number))
+    {
+        printf("AMEX\n");
+        return 0;
+    }
+    if (check_visa(length, cc_number))
+    {
+        printf("VISA\n");
+        return 0;
+    }
+    if (check_mastercard(length, cc_number))
+    {
+        printf("MASTERCARD\n");
+        return 0;
+    }
+    printf("INVALID\n");
+    return 0;
+}
+
+int get_length(long n)
+{
+    int counter = 0;
+    while (n > 0)
+    {
+        n /= 10;
+        ++counter;
+    }
+    return counter;
+}
+
+bool check_sum(long n)
+{
+    int even = 0;
+    int odd = 0;
+    int check = 0;
+    while (n > 0)
+    {
+        // odd numbers
+        int x = n % 10;
+        odd += x;
+        n /= 10;
+
+        // even numbers
+        int y = (n % 10) * 2;
+        while (y > 0)
+        {
+            even += y % 10;
+            y /= 10;
+        }
+        n /= 10;
+    }
+    check = even + odd;
+    if (!(check % 10 == 0))
+    {
+        return false;
+    }
+    return true;
+}
+
+bool check_amex(int length, long cc)
+{
+    if (!(length == 15))
+    {
+        return false;
+    }
+    int front_two = cc / 10000000000000;
+    if ((front_two == 34 || front_two == 37))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool check_visa(int length, long cc)
+{
+    if (!(length == 13 || length == 16))
+    {
+        return false;
+    }
+    if (length == 16 && cc / 1000000000000000 == 4)
+    {
+        return true;
+    }
+    if (length == 13 && (cc / 1000000000000 == 4))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool check_mastercard(int length, long cc)
+{
+    if (!(length == 16))
+    {
+        return false;
+    }
+    int front_two = cc / 100000000000000;
+    if ((front_two < 51 || front_two > 55))
+    {
+        return false;
+    }
+    return true;
 }
