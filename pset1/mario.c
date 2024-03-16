09ffab80ac8a286ad1fbb20b7ec701208bf1acac
Abbie Miller <106941149+abbierm@users.noreply.github.com>
2024-03-16 16:41:15 -0500

    Update mario.c

--- pset1/mario.c
+++ pset1/mario.c
@@ -1,42 +1,32 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
     int height;
-
-    // Makes sure user puts in valid character
     do
     {
-        // Ask user for height
-        height = get_int("Pick a height between 1 and 8: ");
+        height = get_int("Height: ");
     }
-    while (height < 1 || height > 8);
+    while (height <= 0 || height > 8);
 
-    // For loop to print the mario pyramid
-
-    for (int i = 0; i < height; i++)
+    for (int i = 1; i <= height; i++)
     {
-        // Prints the alignment spaces
-        for (int alignmentSpaces = height - 1; alignmentSpaces > i; alignmentSpaces--)
+        for (int as = height - i; as > 0; as--)
         {
             printf(" ");
         }
-        // Prints the hashes
-        for (int j = height ; j >= height - i ; j--)
+        for (int j = 0; j < i; j++)
         {
             printf("#");
         }
-        // Spaces in between the hashes
         printf("  ");
 
-        // Prints next row of hashes
-        for (int q = height ; q >= height - i; q--)
+        for (int x = 0; x < i; x++)
         {
             printf("#");
         }
         printf("\n");
-
     }
-
 }
