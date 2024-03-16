b2bfd5a0467426dcb48989c75fe3071404f6f48b
Abbie Miller <106941149+abbierm@users.noreply.github.com>
2024-03-16 16:42:12 -0500

    Update readability.c

--- pset2/readability.c
+++ pset2/readability.c
@@ -1,85 +1,83 @@
 #include <cs50.h>
-#include <stdio.h>
-#include <string.h>
+#include <ctype.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int count_letters(string t);
-int count_words(string text);
-int count_sentences(string text);
+float count_letters(string text);
+float count_words(string text);
+float count_sentences(string text);
+int calculate_index(float words, float sentences, float letters);
 
 int main(void)
 {
-// Prompts user for a string of text using get_string
-    string copied_text = get_string("Text:");
-
-// Calls on the functions
-    float letters = count_letters(copied_text);
-    float words = count_words(copied_text);
-    float sentences = count_sentences(copied_text);
-
-// Use formula to get the reading index
-    float L = (letters / words) * 100;
-    float S = (sentences / words) * 100;
-    int reading_index = round(.0588 * L - .296 * S - 15.8);
-
-// Print out grade level using the index
-    if (reading_index < 16 && reading_index >= 1)
+    string text = get_string("Text: ");
+    float letters = count_letters(text);
+    float words = count_words(text);
+    float sentences = count_sentences(text);
+    int index = calculate_index(words, sentences, letters);
+    if (index > 0 && index <= 16)
     {
-        printf("Grade %i\n", reading_index);
+        printf("Grade %i\n", index);
     }
-    else if (reading_index >= 16)
-    {
-        printf("Grade 16+\n");
-    }
-    else if (reading_index < 1)
+    else if (index < 1)
     {
         printf("Before Grade 1\n");
     }
-}
-
-// Counts the number of letters
-int count_letters(string t)
-{
-    int number_letters = 0;
-    for (int i = 0, n = strlen(t); i < n; i++)
+    else
     {
-        if (t[i] >= 'A' && t[i] <= 'Z')
-        {
-            number_letters++;
-        }
-        else if (t[i] >= 'a' && t[i] <= 'z')
-        {
-            number_letters++;
-        }
+        printf("Grade 16+\n");
     }
-    return number_letters;
 }
 
-// Counts the numbers of words
-int count_words(string text)
+float count_letters(string text)
 {
-    // 1 because the last word won't have a space behind it.
-    int number_words = 1;
+    float letters = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if (text[i] == ' ')
+        char c = toupper(text[i]);
+        if (c >= 65 && c <= 90)
         {
-            number_words++;
+            letters += 1;
         }
     }
-    return number_words;
+    return letters;
 }
 
-// Counts the number of sentences
-int count_sentences(string text)
+float count_words(string text)
 {
-    int number_sentences = 0;
+    float words = 1;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+        char c = text[i];
+        if (c == ' ')
         {
-            number_sentences ++;
+            words += 1;
         }
     }
-    return number_sentences;
+    return words;
+}
+
+float count_sentences(string text)
+{
+    float sentences = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        char c = text[i];
+        if (c == '.' || c == '?' || c == '!')
+        {
+            sentences += 1;
+        }
+    }
+    return sentences;
+}
+
+int calculate_index(float words, float sentences, float letters)
+{
+    float avg_letters = (letters / words) * 100;
+    float avg_sentences = (sentences / words) * 100;
+    float index = (.0588 * avg_letters) - (.296 * avg_sentences) - 15.8;
+    int i = round(index);
+    return i;
 }
