87298f02e1f7b529fcd94416595bb27bf08bac55
Abbie Miller <106941149+abbierm@users.noreply.github.com>
2024-06-19 15:19:21 -0500

    Update 11.sql

--- pset7/movies/11.sql
+++ pset7/movies/11.sql
@@ -1,6 +1,11 @@
--- Write a SQL query to list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated.
--- Your query should output a table with a single column for the title of each movie
--- You may assume that there is only one person in the database with the name Chadwick Boseman
+/*
+Write a SQL query to list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated.
+
+    - Your query should output a table with a single column for the title of
+        each movie.
+    - You may assume that there is only one person in the database with the
+        name Chadwick Boseman.
+*/
 SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
