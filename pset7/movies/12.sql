d6e68c6c409d913d432212391a5c21597d030e89
Abbie Miller <106941149+abbierm@users.noreply.github.com>
2024-06-19 15:19:55 -0500

    Update 12.sql

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,11 +1,17 @@
--- Write a SQL query to list the titles of all movies in which both Johnny Depp and Helena Bonham Carter starred.
+/*
+Write a SQL query to list the titles of all movies in which both Bradley Cooper and Jennifer Lawrence starred.
+
+    - Your query should output a table with a single column for the title of each movie.
+    - You may assume that there is only one person in the database with the name Bradley Cooper.
+    - You may assume that there is only one person in the database with the name Jennifer Lawrence.
+*/
 SELECT title FROM movies, stars, people
-WHERE name = "Johnny Depp"
+WHERE name = "Bradley Cooper"
 AND people.id = stars.person_id
 AND stars.movie_id = movies.id
 
 INTERSECT
 SELECT title FROM movies, stars, people
-WHERE name = "Helena Bonham Carter"
+WHERE name = "Jennifer Lawrence"
 AND people.id = stars.person_id
 AND stars.movie_id = movies.id;
