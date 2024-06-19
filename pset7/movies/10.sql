b9296fc8e549c397ef4f8b072f543b7c05563897
Abbie Miller <106941149+abbierm@users.noreply.github.com>
2024-06-19 15:18:56 -0500

    Update 10.sql

--- pset7/movies/10.sql
+++ pset7/movies/10.sql
@@ -1,4 +1,11 @@
--- Write a SQL query to list the names of all people who have directed a movie that received a rating of at least 9.0.
+/*
+Write a SQL query to list the names of all people who have directed a movie that received a rating of at least 9.0.
+
+    - Your query should output a table with a single column for the name of
+        each person.
+    - If a person directed more than one movie that received a rating of at
+        least 9.0, they should only appear in your results once.
+*/
 SELECT DISTINCT name FROM people, ratings, directors, movies
 WHERE movies.id = ratings.movie_id
 AND rating >= 9.0
