ba7e6d4790321d21b080601e1da58cc5a249298d
Abbie Miller <106941149+abbierm@users.noreply.github.com>
2024-12-23 14:43:24 -0600

    Update caesar.c
    refactored for 7b+

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -1,87 +1,72 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-int rotate(char c, int rotate_number);
+
+int rotate(char c, int k);
 
 int main(int argc, string argv[])
 {
-    // if int argc != 2, print an error message and and return 1
     if (argc != 2)
     {
-        printf("Program needs an integer value and you gave %i\n", argc - 1);
+        printf("Usage: ./caesar key\n");
         return 1;
     }
 
-    // Input validation to make sure the argument is a digit;
+    int key = -1;
     for (int i = 0, n = strlen(argv[1]); i < n; i++)
     {
-        if (isdigit(argv[1][i]))
+        if (!isdigit(argv[1][i]))
         {
-            continue;
+            printf("Error: key\n");
+            return 2;
+        }
+    }
+    key = atoi(argv[1]) % 26;
+
+    string text = get_string("Plain text:  ");
+
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (!isalpha(text[i]))
+        {
+            printf("%c", text[i]);
         }
         else
         {
-            printf("Usage: ./caesar key\n");
-            return 1;
+            char c = text[i];
+            int upper_flag = 0;
+            if (isupper(c))
+            {
+                upper_flag = 1;
+                c = tolower(c);
+            }
+            c = rotate(c, key);
+            if (upper_flag == 1)
+            {
+                printf("%c", toupper(c));
+            }
+            else
+            {
+                printf("%c", c);
+            }
         }
     }
-    int key = atoi(argv[1]) % 26;
-
-    // Gets plain text string
-    string plain_text = get_string("Plain text:  ");
-    printf("ciphertext: ");
-
-    // Use rotate function
-    // n needs to be less than strlen so we don't include the null char
-    for (int i = 0, n = strlen(plain_text); i < n; i++)
-    {
-        char letter = plain_text[i];
-        int ciphertext = rotate(letter, key);
-
-        // Output
-        printf("%c", ciphertext);
-    }
-
     printf("\n");
+    return 0;
 }
 
-//function that moves the char by the digits the user provided
-int rotate(char c, int rotate_number)
+char rotate(char c, int k)
 {
-    if (c >= 65 && c <= 90)
+    if ((c + k) <= 122)
     {
-        int new_upper_char = c + rotate_number;
-        if (new_upper_char > 90)
-        {
-            int difference = new_upper_char - 90;
-            new_upper_char = 64 + difference;
-            return new_upper_char;
-        }
-        else
-        {
-            return new_upper_char;
-        }
-    }
-    else if (c > 96 && c <= 122)
-    {
-        int new_lower_char = c + rotate_number;
-        if (new_lower_char > 122)
-        {
-            int upper_diff = new_lower_char - 122;
-            new_lower_char =  96 + upper_diff;
-            return new_lower_char;
-        }
-        else
-        {
-            return new_lower_char;
-        }
+        return c + k;
     }
     else
     {
-        return c;
+       int newc = ((k + c) - 123) + 97;
+       return newc;
     }
-
 }
